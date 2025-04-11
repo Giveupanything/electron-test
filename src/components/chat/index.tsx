b9e3d9d8cff37983de95d85e1e4542301832504b
kezhendong <kezhendong@moresec.cn>
2025-04-11 17:37:24 +0800

    feat: 智能问答菜单添加折叠

--- src/components/chat/index.tsx
+++ src/components/chat/index.tsx
@@ -1,48 +1,80 @@
-import { useChatContext } from '@/context/chat-context';
-import Sidebar from './sidebar';
-import ConfigPanel from './config-panel';
-import Loading from '../UI/loading';
-import ChatWrapper from './chat-wrapper';
+import { useChatContext } from "@/context/chat-context";
+import Sidebar from "./sidebar";
+import ConfigPanel from "./config-panel";
+import Loading from "../UI/loading";
+import ChatWrapper from "./chat-wrapper";
+import ExpandIcon from "@/assets/icons/epand.svg";
+import classNames from "classnames";
+import { useUpdate } from "ahooks";
+
+const KEY = "chat-sidebar-fold";
 
 export default function ChatPage() {
+  const {
+    apiKey,
+    showConfigPanelBeforeChat,
+    chatTree,
+    loadingChatList,
+    loading,
+    currentConversationId,
+  } = useChatContext();
 
-  const { apiKey, showConfigPanelBeforeChat, chatTree, loadingChatList, loading, currentConversationId } = useChatContext();
+  const update = useUpdate();
 
-  const chatReady = (!showConfigPanelBeforeChat || !!chatTree.length);
+  const chatReady = !showConfigPanelBeforeChat || !!chatTree.length;
 
-  if(loading) return <Loading type="app" />;
+  const fold = Boolean(localStorage.getItem(KEY));
+  const setFold = () => {
+    update();
+    if (fold) {
+      localStorage.removeItem(KEY);
+    } else {
+      localStorage.setItem(KEY, "true");
+    }
+  };
+
+  if (loading) return <Loading type="app" />;
 
   return (
     <div
       className="h-full flex"
       style={{
-        backgroundImage: (chatReady && !loadingChatList) ? 'linear-gradient(180deg,rgba(249,250,251,.9),rgba(242,244,247,.9) 90.48%)' : 'none'
+        backgroundImage:
+          chatReady && !loadingChatList
+            ? "linear-gradient(180deg,rgba(249,250,251,.9),rgba(242,244,247,.9) 90.48%)"
+            : "none",
       }}
     >
       {/* 侧边栏 */}
-      <Sidebar apiKey={apiKey} />
+      <Sidebar apiKey={apiKey} fold={fold} />
 
       {/* 内容区 */}
-      <div className={`overflow-hidden flex-1 rounded-md w-full h-full min-w-[750px] ${showConfigPanelBeforeChat && !chatTree.length && 'flex items-center justify-center'}`}>
-        {
-          showConfigPanelBeforeChat && !loadingChatList && !chatTree.length && (
-            <div className={`flex w-full items-center justify-center h-full`}>
-              <ConfigPanel />
-            </div>
-          )
-        }
+      <div
+        className={`overflow-hidden flex-1 rounded-md w-full h-full min-w-[750px] ${showConfigPanelBeforeChat && !chatTree.length && "flex items-center justify-center"} relative`}
+      >
+        <div
+          className={classNames(
+            "absolute  left-2 top-4 border border-[rgba(0,0,0,0.2)] rounded p-0.5 hover:opacity-70 z-10 cursor-pointer",
+            {
+              "rotate-180": !fold,
+            }
+          )}
+          onClick={() => setFold(!fold)}
+        >
+          <img src={ExpandIcon} alt="" />
+        </div>
 
-        {
-          loadingChatList && chatReady && (
-            <Loading type="app" />
-          )
-        }
+        {showConfigPanelBeforeChat && !loadingChatList && !chatTree.length && (
+          <div className={`flex w-full items-center justify-center h-full`}>
+            <ConfigPanel />
+          </div>
+        )}
 
-        {
-          chatReady && !loadingChatList && (
-            <ChatWrapper key={currentConversationId} />
-          )
-        }
+        {loadingChatList && chatReady && <Loading type="app" />}
+
+        {chatReady && !loadingChatList && (
+          <ChatWrapper key={currentConversationId} />
+        )}
       </div>
     </div>
   );
--- src/components/chat/sidebar/index.tsx
+++ src/components/chat/sidebar/index.tsx
@@ -1,74 +1,97 @@
-import { ConversationItem } from '@/typings/app';
-import { EditOutlined } from '@ant-design/icons';
-import { Button, message } from 'antd';
-import List from './list';
-import { useSetState } from 'ahooks';
-import Confirm from '@/components/base/confirm';
-import { useCallback } from 'react';
-import { delConversation, renameConversation } from '@/api/chat';
-import RenameModal from './rename-modal';
-import { useChatContext } from '@/context/chat-context';
-import { DEFAULT_USER_ID } from '@/config';
+import { ConversationItem } from "@/typings/app";
+import { EditOutlined } from "@ant-design/icons";
+import { Button, message } from "antd";
+import List from "./list";
+import { useSetState } from "ahooks";
+import Confirm from "@/components/base/confirm";
+import { useCallback } from "react";
+import { delConversation, renameConversation } from "@/api/chat";
+import RenameModal from "./rename-modal";
+import { useChatContext } from "@/context/chat-context";
+import { DEFAULT_USER_ID } from "@/config";
+import classNames from "classnames";
 
 type Props = {
-    apiKey: string
-}
+  fold: boolean;
+  apiKey: string;
+};
 
-export default function Sidebar({ apiKey }: Props) {
-
-  const { currentConversationId, conversationList, handleChangeConversation, handleNewConversation, setConversationName, handleDelete: handleDeleteConversation } = useChatContext();
+export default function Sidebar({ apiKey, fold }: Props) {
+  const {
+    currentConversationId,
+    conversationList,
+    handleChangeConversation,
+    handleNewConversation,
+    setConversationName,
+    handleDelete: handleDeleteConversation,
+  } = useChatContext();
 
   const [state, setState] = useSetState({
     showConfirm: null as ConversationItem | null,
     showRename: null as ConversationItem | null,
-    loading: false
+    loading: false,
   });
   const { showConfirm, showRename, loading } = state;
 
   const handleOperate = useCallback((type: string, item: ConversationItem) => {
-    if(type === 'delete') setState({ showConfirm: item });
-    if(type === 'rename') setState({ showRename: item });
+    if (type === "delete") setState({ showConfirm: item });
+    if (type === "rename") setState({ showRename: item });
   }, []);
 
   const handleDelete = useCallback(async () => {
-    if(!showConfirm || loading) return;
+    if (!showConfirm || loading) return;
 
     try {
       setState({ loading: true });
-      await delConversation(showConfirm?.id || '', { apiKey, user: DEFAULT_USER_ID });
-      handleDeleteConversation(showConfirm?.id || '');
-      message.success('删除成功');
+      await delConversation(showConfirm?.id || "", {
+        apiKey,
+        user: DEFAULT_USER_ID,
+      });
+      handleDeleteConversation(showConfirm?.id || "");
+      message.success("删除成功");
       setState({ showConfirm: null });
-    } catch(error) {
-      console.log('error:>> ', error);
-      message.error('删除失败');
+    } catch (error) {
+      console.log("error:>> ", error);
+      message.error("删除失败");
     } finally {
       setState({ loading: false });
     }
-
   }, [showConfirm, loading]);
 
-  const handleRename = useCallback(async (name: string) => {
-    if(!showRename || loading) return;
-    if(!name) return message.error('请先输入名称');
+  const handleRename = useCallback(
+    async (name: string) => {
+      if (!showRename || loading) return;
+      if (!name) return message.error("请先输入名称");
 
-    try {
-      setState({ loading: true });
-      await renameConversation(showRename?.id || '', { apiKey, name, user: DEFAULT_USER_ID });
-      setConversationName(showRename?.id || '', name);
-      message.success('操作成功');
-      setState({ showRename: null });
-    } catch(error) {
-      console.log('error:>> ', error);
-      message.error('操作失败');
-    } finally {
-      setState({ loading: false });
-    }
-
-  }, [showRename, loading]);
+      try {
+        setState({ loading: true });
+        await renameConversation(showRename?.id || "", {
+          apiKey,
+          name,
+          user: DEFAULT_USER_ID,
+        });
+        setConversationName(showRename?.id || "", name);
+        message.success("操作成功");
+        setState({ showRename: null });
+      } catch (error) {
+        console.log("error:>> ", error);
+        message.error("操作失败");
+      } finally {
+        setState({ loading: false });
+      }
+    },
+    [showRename, loading]
+  );
 
   return (
-    <div className="w-1/6 max-w-[200px] bg-white border-r border-r-[rgba(0,0,0,0.1)] min-w-[135px]">
+    <div
+      className={classNames(
+        "w-1/6 max-w-[200px] bg-white border-r border-r-[rgba(0,0,0,0.1)] min-w-[135px] relative duration-500",
+        {
+          "!w-0 !min-w-0 overflow-hidden opacity-0": !fold,
+        }
+      )}
+    >
       {/* bg-[#f7f9fb] */}
       <div className="shrink-0 p-4">
         <Button
@@ -76,22 +99,19 @@ export default function Sidebar({ apiKey }: Props) {
           icon={<EditOutlined />}
           onClick={handleNewConversation}
         >
-            开启新对话
+          开启新对话
         </Button>
       </div>
 
       <div className="grow px-4 py-2 overflow-y-scroll pb-8 h-[calc(100%-64px)]">
-        {
-          !!conversationList.length &&
-          (
-            <List
-              list={conversationList}
-              onChangeConversation={handleChangeConversation}
-              onOperate={handleOperate}
-              currentConversationId={currentConversationId}
-            />
-          )
-        }
+        {!!conversationList.length && (
+          <List
+            list={conversationList}
+            onChangeConversation={handleChangeConversation}
+            onOperate={handleOperate}
+            currentConversationId={currentConversationId}
+          />
+        )}
       </div>
 
       {!!showConfirm && (
@@ -109,7 +129,7 @@ export default function Sidebar({ apiKey }: Props) {
           isShow
           onClose={() => setState({ showRename: null })}
           saveLoading={loading}
-          name={showRename?.name || ''}
+          name={showRename?.name || ""}
           onSave={handleRename}
         />
       )}
